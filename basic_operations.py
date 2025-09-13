5f96deb04be2a12952fa0b9bb51ee2a32c1a0a16
StackOdyssey <davidsmithny01@gmail.com>
2025-09-13 19:05:39 +0100

    net test

--- basic_operations.py
+++ basic_operations.py
@@ -8,3 +8,4 @@ multiplication = number1 * number2
 print("Addition  of ", number1, "and ", number2, " is ",  addition )
 print("Subtraction  of ", number1, "and ", number2, " is ",  subtraction)
 print("Multiplication  of ", number1, "and ", number2, " is ",  multiplication)
+print(addition)
