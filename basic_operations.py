f33be070ce3d4581c4ee092bd0e276e56e9f626c
StackOdyssey <davidsmithny01@gmail.com>
2025-09-13 17:49:32 +0100

    added my some opretions

--- basic_operations.py
+++ basic_operations.py
@@ -1,6 +1,10 @@
 number1 = 10
 number2 = 5
 
-result = number1 + number2
+addition  = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
 
-print("The sum of number1", number1, "and number2", number2, " is ",  result)
+print("Addition  of ", number1, "and ", number2, " is ",  addition )
+print("Subtraction  of ", number1, "and ", number2, " is ",  subtraction)
+print("Multiplication  of ", number1, "and ", number2, " is ",  multiplication)
