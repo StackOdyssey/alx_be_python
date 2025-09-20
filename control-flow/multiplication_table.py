de5e9836b44a361cae13340b6e367d1f4b9080e0
StackOdyssey <davidsmithny01@gmail.com>
2025-09-20 16:07:37 +0100

    adding tables version 3

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,4 +2,4 @@ number = int(input("Enter a number to see its multiplication table:"))
 
 for x in range(1, 11):
     Z = number * x
-    print(number, " * ", x, " = ",  Z)
+    print(f"{number} * {x} = {Z}")
