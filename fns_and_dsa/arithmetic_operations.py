cc20ac151e4e11e30bf136a45f74f25fc60184a8
StackOdyssey <davidsmithny01@gmail.com>
2025-09-28 00:50:23 +0100

    writing the script for main.py and arethmetics

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,18 +1,4 @@
 def perform_operation(a, b, operation):
-    """
-    Perform a basic arithmetic operation on two numbers.
-
-    Parameters:
-    a (float): The first number.
-    b (float): The second number.
-    operation (str): The operation to perform. Can be 'add', 'subtract', 'multiply', or 'divide'.
-
-    Returns:
-    float: The result of the arithmetic operation.
-
-    Raises:
-    ValueError: If an unsupported operation is provided or if division by zero is attempted.
-    """
     if operation == 'add':
         return a + b
     elif operation == 'subtract':
