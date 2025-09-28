510fe3036ce6a866f2558ed4870f0cf9ab29fd58
StackOdyssey <davidsmithny01@gmail.com>
2025-09-28 14:49:12 +0100

    creating the exercice nr 2

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -16,10 +16,4 @@ def perform_operation(num1, num2, operation):
     else:
         return "ERROR: Invalid operation"
 
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
-operation = (input("Enter the operation (add, subtract, multiply, divide): ")).lower()
-
-result = perform_operation(num1, num2, operation)
-print(f"Result: {result}")
 
