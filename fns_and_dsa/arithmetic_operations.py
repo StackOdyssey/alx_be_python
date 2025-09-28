80b0c850b9d8832e09a8912228cfb1734d449681
StackOdyssey <davidsmithny01@gmail.com>
2025-09-28 13:41:14 +0100

    writing the full script

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,13 +1,25 @@
+
+
 def perform_operation(num1, num2, operation):
-    if operation == 'add':
+    
+    if operation == "add":
         return num1 + num2
-    elif operation == 'subtract':
+    elif operation == "subtract":
         return num1 - num2
-    elif operation == 'multiply':
+    elif operation == "multiply":
         return num1 * num2
-    elif operation == 'divide':
+    elif operation == "divide":
         if num2 == 0:
-            raise ValueError("Cannot divide by zero.")
-        return num1 / num2
+            return "ERROR: division by Zero"
+        else:
+            return num1 / num2
     else:
-        raise ValueError(f"Unsupported operation: {operation}")
+        return "ERROR: Invalid operation"
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = (input("Enter the operation (add, subtract, multiply, divide): ")).lower()
+
+result = perform_operation(num1, num2, operation)
+print(f"Result: {result}")
+
