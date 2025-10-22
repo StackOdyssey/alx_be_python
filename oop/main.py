3a797b0e5696d3a631ed5cc48d42ae93791a527c
StackOdyssey <davidsmithny01@gmail.com>
2025-10-22 11:01:25 +0100

    finalizing files 3

--- oop/main.py
+++ oop/main.py
@@ -1,19 +1,6 @@
-from book_class import Book
 from library_system import Book, EBook, PrintBook, Library
 
 def main():
-    #Creating an instance of Book
-    my_book = Book("1984", "George Orwell", 1949)
-
-    # Demonstrating the __str__ method
-    print(my_book)  # Expected to use __str__
-
-    # Demonstrating the __repr__ method
-    print(repr(my_book))  # Expected to use __repr__
-
-    # Deleting a book instance to trigger __del__
-    del my_book
-
     #Create a Library instance
     my_library = Library()
 
